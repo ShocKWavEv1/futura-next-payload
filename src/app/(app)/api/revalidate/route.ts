3f08665092a061439543f8796aab9f7462b2430a
ShocKWavEv1 <rodd.sal23@gmail.com>
2024-07-19 16:57:34 -0600

    see how revalidate affects multiple updates

--- src/app/(app)/api/revalidate/route.ts
+++ src/app/(app)/api/revalidate/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: any, res: any) {
   const slug = searchParams.get("slug");
   const secret = searchParams.get("secret");
 
-  // const revalidateCollection: any = `/${slug?.replace(/index/, "")}`;
+  //const revalidateCollection: any = `/${slug?.replace(/index/, "")}`;
 
   if (secret !== process.env.FRONTEND_SECRET) {
     return NextResponse.json({ status: 401, message: "Invalid Token" });
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import { Box, Heading } from '@chakra-ui/react'
-import { getPayloadHMR } from '@payloadcms/next/utilities'
-import configPromise from '@payload-config'
-import { basePadding } from './lib/basePadding'
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import { getPayloadHMR } from "@payloadcms/next/utilities";
+import configPromise from "@payload-config";
+import { basePadding } from "./lib/basePadding";
 
-const payload = await getPayloadHMR({ config: configPromise })
+const payload = await getPayloadHMR({ config: configPromise });
 
 const Page = async () => {
   const categories = await payload.find({
-    collection: 'categories',
+    collection: "categories",
     limit: 100,
-    sort: 'createdAt',
-  })
+    sort: "createdAt",
+  });
+
+  const team = await payload.find({
+    collection: "team",
+    limit: 100,
+    sort: "createdAt",
+  });
 
   return (
-    <Box
-      bg="black"
-      padding={basePadding()}
-      w="100%"
-      h="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
+    <Box bg="black" padding={basePadding()} w="100%" h="100vh">
       <Box
         w="100%"
         h="100%"
@@ -34,12 +32,15 @@ const Page = async () => {
         <Heading variant="H3BOLD" color="white" textAlign="center">
           Hi Futura Next App Router and Payload 3.0
         </Heading>
-        <Heading pt="20px" variant="H5BOLD" color="white" textAlign="center">
-          Fetch: {categories.docs[0]?.text}
+        <Heading pt="20px" variant="H6BOLD" color="white" textAlign="center">
+          Fetch Categories: {categories.docs[0]?.text}
+        </Heading>
+        <Heading pt="20px" variant="H6BOLD" color="white" textAlign="center">
+          Team: {team.docs[0]?.name}
         </Heading>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
