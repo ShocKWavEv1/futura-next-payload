21e25554d18c4ba2eb9712f730b7f2b657934121
ShocKWavEv1 <rodd.sal23@gmail.com>
2024-11-05 19:08:45 -0600

    add revalidate to crew service

--- src/collections/team.ts
+++ src/collections/team.ts
@@ -21,7 +21,7 @@ export const TeamCollection: CollectionConfig = {
     ],
     afterChange: [
       ({ doc }) => {
-        revalidatePage("categories");
+        revalidatePage("team");
       },
     ],
   },
